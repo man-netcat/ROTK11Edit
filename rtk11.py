49bc1861e11c7dd278c343ac6ff0204c2c832824
Rick Teuthof <rmt22198@outlook.com>
2023-02-17 02:01:14 +0100

    Parsedata function

--- rtk11.py
+++ rtk11.py
@@ -2,30 +2,40 @@ import glob
 from constants import *
 
 
+def parsedata(f, offset, lengths, repetitions=1, excess=0):
+    f.seek(offset)
+    if repetitions == 1:
+        data = f.read(lengths)
+    else:
+        data = []
+        for _ in range(repetitions):
+            data.append(f.read(lengths))
+            f.read(excess)
+    return data
+
+
 def parsefile(f):
-    # Attributes are marked as [OFFSET];[LENGTH]x[REPETITION]
+    # Attributes are marked as [OFFSET];[LENGTH]x[REPETITION]~[EXCESS]
 
     # YEAR-00-MONTH-01 (Displayed Starting Date)
     # 91;4
-    f.seek(91)
-    year, _, month, _ = f.read(4)
+    year, _, month, _ = parsedata(f, 91, 4)
     print(year, month)
 
     # Scenario Name.
     # 95;26
-    scenname = f.read(26).decode()
+    scenname = parsedata(f, 95, 26).decode()
     print(scenname)
 
     # Scenario Description.
     # 121;600
-    f.seek(121)
-    tmp = f.read(600).decode()
-    print(tmp)
+    scendesc = parsedata(f, 121, 600).decode()
+    print(scendesc)
 
     # Force Colours.
     # 722;42
     f.seek(722)
-    data = f.read(42)
+    data = parsedata(f, 722, 42)
     colours = [
         colour_map[byte] if byte != 0xFF else 0xFFFFFF
         for byte in data
@@ -33,42 +43,34 @@ def parsefile(f):
     print(colours)
 
     # Force Descriptions.
-    # 931;607x42
-    f.seek(931)
-    for i in range(42):
-        forcedesc = f.read(607).decode()
-        print(i, forcedesc)
+    # 933;607x42
+    forcedescs = parsedata(f, 933, 607, 42)
+    for forcedesc in forcedescs:
+        print(forcedesc.decode())
 
     # YEAR-00-MONTH-01 (In-Game Starting Date)
-    # 26426;4
-    f.seek(26427)
-    year, _, month, _ = f.read(4)
+    # 26427;4
+    year, _, month, _ = parsedata(f, 26427, 4)
     print(year, month)
 
     # District code + Max HP of cities.
     # Max HP is 2 bytes in little endian
     # 15 Unknown bytes left
-    # 26438;(1+2)x86
-    f.seek(26438)
-    for i in range(86):
-        district = f.read(1)
-        maxhp = int.from_bytes(f.read(2), "little")
-        print(i, district, maxhp)
-        f.read(15)
+    # 26438;(1+2)x86~15
+    districthp = parsedata(f, 26438, 3, 86, 15)
+    for d, *hpdata in districthp:
+        hp = int.from_bytes(hpdata, "little")
+        print(d, hp)
 
     # Officer Data.
     # 28003;152x850
-    f.seek(28003)
-    for i in range(850):
-        officerdata = f.read(152)
-        print(i, officerdata)
+    officerdata = parsedata(f, 28003, 152, 850)
+    print(officerdata)
 
     # Item Data
-    # 157203;152x50
-    f.seek(157203)
-    for i in range(50):
-        officerdata = f.read(34)
-        print(i, officerdata)
+    # 157203;34x50
+    itemdata = parsedata(f, 157203, 34, 50)
+    print(itemdata)
 
 
 def main():
