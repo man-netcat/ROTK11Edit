5be51231b0c62d48212291ede5c1c62ceb55d077
Rick Teuthof <rmt22198@outlook.com>
2023-04-18 13:39:39 +0200

    Added items

--- rtk11.py
+++ rtk11.py
@@ -1,17 +1,19 @@
 import glob
+import os
 from pathlib import Path
 
 from binary_parser.binary_parser import BinaryParser
 from constants import *
-import os
 
 
 def main():
     with BinaryParser('rtk11.lyt') as bp:
-        for filename in glob.glob("scenario/SCEN*"):
-            stem = Path(filename).stem
-            os.remove(f'databases/{stem}.db')
-            bp.parse_file(filename, f'databases/{stem}.db')
+        for binary_path in glob.glob("scenario/SCEN*"):
+            db_name = Path(binary_path).stem
+            db_path = f'databases/{db_name}.db'
+            if os.path.exists(db_path):
+                os.remove(db_path)
+            bp.parse_file(binary_path, db_path)
 
 
 if __name__ == '__main__':
