f14f33edfa0191ec3c2fe296d453d0ff53328a7e
Rick Teuthof <rmt22198@outlook.com>
2023-04-20 10:29:37 +0200

    Write-back changes

--- rtk11.py
+++ rtk11.py
@@ -1,6 +1,7 @@
 import glob
 import os
 from pathlib import Path
+import shutil
 
 from binary_parser.binary_parser import BinaryParser
 from constants import *
@@ -8,12 +9,17 @@ from constants import *
 
 def main():
     with BinaryParser('rtk11.lyt') as bp:
-        for binary_path in glob.glob("scenario/SCEN*"):
-            db_name = Path(binary_path).stem
-            db_path = f'databases/{db_name}.db'
+        for binary_path in sorted(glob.glob("scenario/SCEN*")):
+            scenario_name = Path(binary_path).stem
+            new_binary_path = binary_path.replace(
+                scenario_name, f"new_{scenario_name}")
+            shutil.copyfile(binary_path, new_binary_path)
+            db_path = f'databases/{scenario_name}.db'
             if os.path.exists(db_path):
                 os.remove(db_path)
             bp.parse_file(binary_path, db_path)
+            # Should result in identical file
+            bp.write_back(new_binary_path, db_path)
 
 
 if __name__ == '__main__':
