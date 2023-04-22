cebc6bdb60257805e500b0efe62f81c769d56f6e
Rick Teuthof <rmt22198@outlook.com>
2023-04-22 22:22:03 +0200

    Back to what works

--- rtk11.py
+++ rtk11.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import sqlite3
 import sys
-from pathlib import Path
+import tempfile
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
@@ -26,27 +26,46 @@ class TableGUI(QMainWindow):
 
         self.file_menu = self.menubar.addMenu("&File")
 
-        open_file_action = QAction("&Open Database", self)
-        open_file_action.setShortcut("Ctrl+O")
-        open_file_action.triggered.connect(self.open_database)
-        self.file_menu.addAction(open_file_action)
+        # Add "Open Database" action
+        self.open_file_action = QAction("&Open Database", self)
+        self.open_file_action.setShortcut("Ctrl+O")
+        self.open_file_action.triggered.connect(self.open_database)
+        self.file_menu.addAction(self.open_file_action)
 
-        save_file_action = QAction("&Save Database", self)
-        save_file_action.setShortcut("Ctrl+S")
-        save_file_action.triggered.connect(self.save_database)
-        self.file_menu.addAction(save_file_action)
+        # Add "Save Database" action
+        self.save_file_action = QAction("&Save Database", self)
+        self.save_file_action.setShortcut("Ctrl+S")
+        self.save_file_action.triggered.connect(self.save_database)
+        self.save_file_action.setEnabled(False)
+        self.file_menu.addAction(self.save_file_action)
+
+        # Add "Save As Database" action
+        self.save_as_file_action = QAction("Save &As Database", self)
+        self.save_as_file_action.setShortcut("Ctrl+Shift+S")
+        self.save_as_file_action.triggered.connect(self.save_as_database)
+        self.save_as_file_action.setEnabled(False)
+        self.file_menu.addAction(self.save_as_file_action)
+
+        # Add "Exit" action
+        self.exit_action = QAction("&Exit", self)
+        self.exit_action.setShortcut("Ctrl+Q")
+        self.exit_action.triggered.connect(self.close)
+        self.file_menu.addAction(self.exit_action)
 
         self.tables = []
+        self.new_scen_path = None
 
     def open_database(self):
+        """Opens a scenario file and parses the data into a database file for editing.
+        """
         self.old_scen_path, _ = QFileDialog.getOpenFileName(
             self, "Open Scenario File", "", "Scenario Files (*.s11)")
+
         if not self.old_scen_path:
             return
 
-        scenario_name = Path(self.old_scen_path).stem
+        self.db_path = os.path.join(tempfile.gettempdir(), 'rtk11.db')
 
-        self.db_path = f'databases/{scenario_name}.db'
         if os.path.exists(self.db_path):
             os.remove(self.db_path)
 
@@ -61,7 +80,6 @@ class TableGUI(QMainWindow):
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         table_names = [x[0] for x in cursor.fetchall()]
 
-        # Create a table widget for each table in the database
         for table_name in table_names:
             table_widget = QTableWidget(self)
             table_widget.setObjectName(table_name)
@@ -74,21 +92,30 @@ class TableGUI(QMainWindow):
             data = cursor.fetchall()
 
             # Populate the table widget with the data
-            self.create_table_widget(headers, data, table_widget)
+            table_widget.setColumnCount(len(headers))
+            table_widget.setHorizontalHeaderLabels(headers)
+            table_widget.setRowCount(len(data))
+            for i in range(len(data)):
+                for j in range(len(headers)):
+                    item = QTableWidgetItem(str(data[i][j]))
+                    item.setFlags(item.flags() | Qt.ItemIsEditable)
+                    table_widget.setItem(i, j, item)
 
         conn.close()
+        self.save_file_action.setEnabled(True)
+        self.save_as_file_action.setEnabled(True)
 
     def save_database(self):
-        self.new_scen_path, _ = QFileDialog.getSaveFileName(
-            self, "Save Scenario File", "", "Scenario Files (*.s11)")
+        """
+        Saves the current state of the database to the scenario file.
+        Will first prompt for the path to save to if it doesn't exist yet
+        """
         if not self.new_scen_path:
+            self.save_as_database()
             return
-
-        shutil.copyfile(self.old_scen_path, self.new_scen_path)
 
         conn = sqlite3.connect(self.db_path)
 
-        # Update the data in the database from the table widgets
         for table_widget in self.tables:
             table_name = table_widget.objectName()
             headers = [table_widget.horizontalHeaderItem(
@@ -108,15 +135,18 @@ class TableGUI(QMainWindow):
         with BinaryParser('rtk11.lyt') as self.bp:
             self.bp.write_back(self.new_scen_path, self.db_path)
 
-    def create_table_widget(self, headers, data, table_widget):
-        table_widget.setColumnCount(len(headers))
-        table_widget.setHorizontalHeaderLabels(headers)
-        table_widget.setRowCount(len(data))
-        for i in range(len(data)):
-            for j in range(len(headers)):
-                item = QTableWidgetItem(str(data[i][j]))
-                item.setFlags(item.flags() | Qt.ItemIsEditable)
-                table_widget.setItem(i, j, item)
+    def save_as_database(self):
+        """
+        Prompts the user for a scenario file before calling the regular save function.
+        """
+        self.new_scen_path, _ = QFileDialog.getSaveFileName(
+            self, "Save Scenario File", "", "Scenario Files (*.s11)")
+
+        if not self.new_scen_path:
+            return
+
+        shutil.copyfile(self.old_scen_path, self.new_scen_path)
+        self.save_database()
 
 
 def main():
