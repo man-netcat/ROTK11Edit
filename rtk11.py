8b46387320ea85bbc808c92b5397b769d76fd10d
Rick Teuthof <rmt22198@outlook.com>
2023-05-06 22:52:55 +0200

    Working on research values

--- rtk11.py
+++ rtk11.py
@@ -6,14 +6,9 @@ import tempfile
 from functools import reduce
 from sqlite3 import connect
 
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
-from PyQt5.QtGui import QBrush, QColor, QIcon, QStandardItemModel, QTextOption
-from PyQt5.QtWidgets import (QAction, QApplication, QCheckBox, QComboBox,
-                             QDialog, QFileDialog, QHBoxLayout, QInputDialog,
-                             QLabel, QLineEdit, QMainWindow, QProgressDialog,
-                             QPushButton, QScrollArea, QTableView,
-                             QTableWidget, QTableWidgetItem, QTabWidget,
-                             QToolBar, QVBoxLayout, QWidget)
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 from binary_parser.binary_parser import BinaryParser
 from constants import *
@@ -82,6 +77,18 @@ class ROTKXIGUI(QMainWindow):
         self.exit_action.triggered.connect(self.close)
         self.file_menu.addAction(self.exit_action)
 
+        # Add "Help" menu
+        self.help_menu = self.menubar.addMenu("&Help")
+
+        # Add "About" action
+        self.about_action = QAction("&About", self)
+        self.about_action.triggered.connect(self.show_about_dialog)
+        self.help_menu.addAction(self.about_action)
+
+    def show_about_dialog(self):
+        about_text = "Made by Rick/Catto\n\nhttps://github.com/rickt1998/rtk11edit"
+        QMessageBox.about(self, "About", about_text)
+
     def init_functions(self):
         self.filter_toolbar = QToolBar(self)
         self.filter_toolbar.setWindowTitle("Filter")
@@ -118,38 +125,33 @@ class ROTKXIGUI(QMainWindow):
 
     def init_cell(self, cell_data, table_name, col_name):
         cell_item = QTableWidgetItem()
+        cell_item.setFlags(cell_item.flags() & ~Qt.ItemIsEditable)
+
         if col_name == "colour":
             color = QColor(colour_map[cell_data])
             cell_text = ''
             cell_item.setBackground(QBrush(color))
         elif col_name == "specialty":
-            specialty_idx = specialty_hex.index(cell_data)
-            cell_text = specialty_options[specialty_idx]
-            cell_item.setFlags(cell_item.flags() & ~Qt.ItemIsEditable)
-        elif col_name == "alliance":
+            cell_text = self.get_specialty_text_from_value(cell_data)
+        elif col_name in ["alliance", "research"]:
             cell_text = "Edit"
-            cell_item.setFlags(cell_item.flags() & ~Qt.ItemIsEditable)
         elif col_name == "force":
-            if cell_data == 0xFF:
-                cell_text = "None"
-            else:
-                ruler_id = self.get_values_by_enum(Force.RULER)[cell_data]
-                cell_text = self.get_officer_name(ruler_id)
-            cell_item.setFlags(cell_item.flags() & ~Qt.ItemIsEditable)
+            cell_text = self.get_force_ruler_name_by_id(cell_data)
         elif col_name in officer_columns:
-            cell_text = self.get_officer_name(cell_data)
-            cell_item.setFlags(cell_item.flags() & ~Qt.ItemIsEditable)
+            cell_text = self.get_officer_name_by_id(cell_data)
         elif col_name in col_map:
             cell_text = col_map[col_name][cell_data]
-            cell_item.setFlags(cell_item.flags() & ~Qt.ItemIsEditable)
         elif self.datatypes[table_name][col_name] == 'int':
             cell_text = int(cell_data)
+            cell_item.setFlags(cell_item.flags() | Qt.ItemIsEditable)
         elif self.datatypes[table_name][col_name] == 'str':
-            cell_text = str(cell_data)
+            cell_text = cell_data.replace('\x00', '')
             cell_item.setTextAlignment(
                 QTextOption.WrapAtWordBoundaryOrAnywhere)
             cell_item.setTextAlignment(Qt.AlignVCenter)
+            cell_item.setFlags(cell_item.flags() | Qt.ItemIsEditable)
         cell_item.setData(Qt.DisplayRole, cell_text)
+
         return cell_item
 
     def init_table_widget(self, table_name):
@@ -173,31 +175,55 @@ class ROTKXIGUI(QMainWindow):
         for row_idx in range(len(table_data)):
             for col_idx, col_name in enumerate(col_names):
                 cell_data = table_data[row_idx][col_idx]
-                if self.datatypes[table_name][col_name] == 'int':
-                    cell_data = int(cell_data)
-                elif self.datatypes[table_name][col_name] == 'str':
-                    # Replace null bytes
-                    cell_data = cell_data.replace('\x00', '')
                 cell_item = self.init_cell(cell_data, table_name, col_name)
                 table_widget.setItem(row_idx, col_idx, cell_item)
-                if "relationship" in col_name or col_name == "unknown":
-                    table_widget.setColumnHidden(col_idx, True)
+
+        for col_idx, col_name in enumerate(col_names):
+            if any([substr in col_name for substr in ["unknown", "relationship"]]):
+                table_widget.setColumnHidden(col_idx, True)
 
         self.tab_widget.addTab(table_widget, table_name)
         self.table_widgets.append(table_widget)
 
     def officer_names(self):
         return [
-            self.get_officer_name(officer_id)
+            self.get_officer_name_by_id(officer_id)
             for officer_id in range(NUM_OFFICERS)]
 
-    def get_officer_name(self, officer_id):
+    def get_specialty_text_from_value(self, value):
+        specialty_idx = specialty_hex.index(value)
+        return specialty_options[specialty_idx]
+
+    def get_specialty_value_from_text(self, text):
+        return reverse(specialty_options)[text]
+
+    def get_force_ruler_name_by_id(self, force_id):
+        if force_id == 0xFF:
+            return "None"
+        ruler_id = self.get_values_by_enum(Force.RULER)[force_id]
+        return self.get_officer_name_by_id(ruler_id)
+
+    def get_force_ruler_id_by_name(self, ruler_name):
+        if ruler_name == "None":
+            return 0xFF
+        officer_id = self.officer_names().index(ruler_name)
+        return self.get_values_by_enum(Force.RULER).index(officer_id)
+
+    def get_officer_name_by_id(self, officer_id):
         if officer_id == 0xFFFF:
             return "None"
         elif officer_id >= 850:  # TODO Shared parent
             return "Parent"
         officer_data = self.table_datas['officer']['data'][officer_id]
         return (officer_data[Officer.FAMILYNAME] + ' ' + officer_data[Officer.GIVENNAME]).replace('\x00', '').strip()
+
+    def get_officer_id_by_name(self, officer_name):
+        if officer_name == "None":
+            return 0xFFFF
+        return self.officer_names().index(officer_name)
+
+    def get_reverse_column_mapping(self, col_name, text):
+        return reverse(col_map[col_name])[text]
 
     def on_key_pressed(self, event):
         table_widget = self.tab_widget.currentWidget()
@@ -223,45 +249,46 @@ class ROTKXIGUI(QMainWindow):
         elif key == Qt.Key_Down and row < table_widget.rowCount() - 1:
             table_widget.setCurrentCell(row + 1, col)
 
-    def on_cell_update(self, item):
+    def on_cell_update(self, cell_item):
         if not self.is_initialized:
             return
-        row_idx = item.row()
-        col_idx = item.column()
-        table_widget = item.tableWidget()
+        row_idx = cell_item.row()
+        col_idx = cell_item.column()
+        table_widget = cell_item.tableWidget()
         table_name = table_widget.objectName()
         col_name = self.table_datas[table_name]['col_names'][col_idx]
-        cell_text = item.text()
-        # print(
-        # f"Cell ({row_idx}, {col_idx}) in {table_name} with column name {col_name} was modified with new value {cell_text}")
+        cell_text = cell_item.text()
 
-        def reverse(d): return {v: k for k, v in d.items()}
-        if col_name == 'id':
+        if col_name in ['id', 'alliance', 'research']:
             return
         elif col_name == 'specialty':
-            cell_data = reverse(specialty_options)[cell_text]
-        elif col_name == 'alliance':
-            return
+            cell_data = self.get_specialty_value_from_text(cell_text)
         elif col_name == "force":
-            if cell_text == "None":
-                cell_data = 0xFF
-            else:
-                officer_id = self.officer_names().index(cell_text)
-                cell_data = self.get_values_by_enum(
-                    Force.RULER).index(officer_id)
+            cell_data = self.get_force_ruler_id_by_name(cell_text)
         elif col_name in officer_columns:
-            if cell_text == "None":
-                cell_data = 0xFFFF
-            else:
-                cell_data = self.officer_names().index(cell_text)
+            cell_data = self.get_officer_id_by_name(cell_text)
         elif col_name in col_map:
-            cell_data = reverse(col_map[col_name])[cell_text]
+            cell_data = self.get_reverse_column_mapping(cell_text)
         elif self.datatypes[table_name][col_name] == 'int':
             cell_data = int(cell_text)
-        elif self.datatypes[table_name][col_name] == 'str':
+        else:
             cell_data = cell_text
 
         self.table_datas[table_name]['data'][row_idx][col_idx] = cell_data
+
+    def get_force_ruler_options(self):
+        ruler_ids = self.get_values_by_enum(Force.RULER)
+        officer_names = self.officer_names()
+        return sorted([officer for officer in officer_names if officer_names.index(officer) in ruler_ids]) + ["None"]
+
+    def get_officer_options(self):
+        sorted(self.officer_names()) + ["None"]
+
+    def get_column_mapping_options(self, col_name):
+        return col_map[col_name].values()
+
+    def get_specialty_options(self):
+        return specialty_options.values()
 
     def on_cell_doubleclick(self, row_idx, col_idx):
         current_tab = self.tab_widget.currentIndex()
@@ -269,25 +296,25 @@ class ROTKXIGUI(QMainWindow):
         cell_item = current_table.item(row_idx, col_idx)
         table_name = current_table.objectName()
         col_name = self.table_datas[table_name]['col_names'][col_idx]
-        if col_name == 'specialty':
-            options = specialty_options.values()
-            self.choose_option(cell_item, options)
-        elif col_name == 'alliance':
-            self.alliance(row_idx)
+
+        if col_name == 'alliance':
+            self.set_alliance_by_row_id(row_idx)
+            return
+        elif col_name == 'research':
+            self.set_research_by_row_id(row_idx)
+            return
+        elif col_name == 'specialty':
+            options = self.get_specialty_options()
         elif col_name == "force":
-            ruler_ids = self.get_values_by_enum(Force.RULER)
-            officer_names = self.officer_names()
-            options = sorted([officer for officer in officer_names if officer_names.index(
-                officer) in ruler_ids]) + ["None"]
-            self.choose_option(cell_item, options)
+            options = self.get_force_ruler_options()
         elif col_name in officer_columns:
-            options = sorted(self.officer_names()) + ["None"]
-            self.choose_option(cell_item, options)
+            options = self.get_officer_options()
         elif col_name in col_map:
-            options = col_map[col_name].values()
-            self.choose_option(cell_item, options)
+            options = self.get_column_mapping_options(col_name)
         else:
             return
+
+        self.choose_option(cell_item, options)
 
     def choose_option(self, cell_item, options):
         combo_box = QComboBox()
@@ -310,7 +337,76 @@ class ROTKXIGUI(QMainWindow):
     def get_values_by_enum(self, enum_value):
         return [x[enum_value] for x in self.table_datas[enum_value.__class__.__name__.lower()]['data']]
 
-    def alliance(self, row_idx):
+    def parse_research_value(self, research_value):
+        return [(research_value >> (28 - i*4)) & 0x0F for i in range(8)]
+
+    def create_research_value(self, research_levels):
+        return sum([level << (28 - i*4) for i, level in enumerate(research_levels)])
+
+    def set_research_by_row_id(self, row_idx):
+        dialog = QDialog()
+        dialog.setWindowTitle("Research Levels")
+
+        research_value = self.table_datas['force']['data'][row_idx][Force.RESEARCH]
+        print(research_value)
+
+        labels = [
+            "Pikes", "Spears", "Horses", "Bows",
+            "Invention", "Command", "Fire", "Defense"]
+
+        levels = [0x0, 0x1, 0x3, 0x7, 0xf]
+
+        research_levels = self.parse_research_value(research_value)
+        print(research_levels)
+
+        sliders_layout = QVBoxLayout()
+        sliders = []
+        for i, label in enumerate(labels):
+            slider_layout = QHBoxLayout()
+            slider_label = QLabel(label)
+            slider = QSlider(Qt.Horizontal)
+            slider.setMinimum(0)
+            slider.setMaximum(4)
+            slider.setValue(levels.index(research_levels[i]))
+            slider.setTickPosition(QSlider.TicksBelow)
+            slider.setTickInterval(1)
+            slider.setSingleStep(1)
+            slider_value_label = QLabel(str(levels.index(research_levels[i])))
+            slider_value_label.setAlignment(Qt.AlignRight)
+            slider_layout.addWidget(slider_label)
+            slider_layout.addWidget(slider)
+            slider_layout.addWidget(slider_value_label)
+            sliders_layout.addLayout(slider_layout)
+            sliders.append(slider)
+
+            def on_slider_value_changed(value, slider_value_label=slider_value_label):
+                slider_value_label.setText(str(value))
+
+            slider.valueChanged.connect(on_slider_value_changed)
+
+        buttons_layout = QHBoxLayout()
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(lambda: dialog.accept())
+        cancel_button = QPushButton("Cancel")
+        cancel_button.clicked.connect(lambda: dialog.reject())
+        buttons_layout.addWidget(ok_button)
+        buttons_layout.addWidget(cancel_button)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(sliders_layout)
+        main_layout.addLayout(buttons_layout)
+
+        dialog.setLayout(main_layout)
+
+        if dialog.exec_() == QDialog.Accepted:
+            new_research_levels = [slider.value() for slider in sliders]
+            print(new_research_levels)
+            new_research_value = self.create_research_value(
+                new_research_levels)
+            print(new_research_value)
+            self.table_datas['force']['data'][row_idx][Force.RESEARCH] = new_research_value
+
+    def set_alliance_by_row_id(self, row_idx):
         dialog = QDialog()
         dialog.setWindowTitle('Alliances')
 
@@ -322,19 +418,19 @@ class ROTKXIGUI(QMainWindow):
         checkboxes_widget = QWidget()
         checkboxes_layout = QVBoxLayout(checkboxes_widget)
 
-        alliance_value = self.table_datas['force'][row_idx][Force.ALLIANCE]
+        alliance_value = self.table_datas['force']['data'][row_idx][Force.ALLIANCE]
 
         force_numbers = self.parse_alliance_value(alliance_value)
         alliance_values = self.get_values_by_enum(Force.ALLIANCE)
         force_rulers = self.get_values_by_enum(Force.RULER)
-        allegiance_values = self.get_values_by_enum(Officer.ALLEGIANCE)
+        # allegiance_values = self.get_values_by_enum(Officer.ALLEGIANCE)
         # ruler_allegiances = [allegiance_values[x]
         #                      if x != 0xFFFF else 0xFF for x in force_rulers]
 
         checkboxes: list[QCheckBox] = []
 
         for i, ruler in enumerate(force_rulers):
-            ruler_name = officer_map[ruler]
+            ruler_name = self.get_officer_name_by_id(ruler)
             checkbox = QCheckBox(f'{ruler_name}')
 
             if i in force_numbers:
@@ -342,7 +438,7 @@ class ROTKXIGUI(QMainWindow):
 
             checkboxes.append(checkbox)
 
-            if ruler_name != 'Unknown':
+            if ruler_name != 'None':
                 checkboxes_layout.addWidget(checkbox)
 
         checkboxes_widget.setLayout(checkboxes_layout)
